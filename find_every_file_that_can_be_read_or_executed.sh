37b9c3f994ce85a1ff62777af98f461acffd314d
jodufour <jodufour@student.42.fr>
2024-12-30 03:10:19 +0100

    feat: added a script to find specific files
    The script finds every file that can be either read or executed

---
+++ find_every_file_that_can_be_read_or_executed.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+BOLD='\e[1m'
+RED='\e[31m'
+RESET='\e[0m'
+ERROR=$BOLD$RED'error'$RESET
+
+if [ $# -ne 1 ]; then
+	echo "$ERROR: wrong number of arguments"
+	echo 'usage: find_every_file_that_can_be_run_or_read.sh DIRECTORY'
+	exit 1
+fi
+
+USER_OWNS_FILE="-user $USER"
+USER_SHARES_A_GROUP_WITH_FILE=$(
+	for gid in $(id -G); do
+		echo -n " -or -group $gid"
+	done | sed "s/^ -or //g"
+)
+
+# Finds every file that satisfies at least one of the following conditions:
+# - the user owns the file
+# - the user shares a group with the file AND at least one of the following conditions is true:
+#   - the file is group-readable
+#   - the file is group-executable
+# - the file is other-readable
+# - the file is other-executable
+find $1 -type f \( $USER_OWNS_FILE -or \( \( $USER_SHARES_A_GROUP_WITH_FILE \) -perm /g+rx \) -or -perm /o+rx \) 2>/dev/null
+
+exit 0
