14df3f86a6181061f8c5e0f4467f1b274d0503c3
jodufour <jodufour@student.42.fr>
2025-01-30 03:36:27 +0100

    feat: automated the steps to get the second flag via a shell script

---
+++ level01/resources/find_readable_passwd_files.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -u
+
+USER_SHARES_A_GROUP_WITH_FILE=$(
+	for GID in $( id -G ) ; do
+		echo -n " -or -group ${GID}"
+	done | sed 's/^ -or //'
+)
+FILE_CAN_BE_READ="( \
+	( -user $USER -exec chmod u+r {} + ) \
+	-or ( ( ${USER_SHARES_A_GROUP_WITH_FILE} ) -perm /g+r ) \
+	-or -perm /o+r \
+)"
+
+# Finds every `passwd` file that satisfies at least one of the following conditions:
+# - the file is owned by the user `level01`
+# - the file shares a group with the user `level01` and is group-readable
+# - the file is other-readable
+# and, for each file owned by the user `level01`, makes them user-readable
+find / -type f -name passwd ${FILE_CAN_BE_READ} -exec ls {} + 2>/dev/null
+
+exit 0
