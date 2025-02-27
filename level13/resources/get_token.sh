ea5c5916fa23bcac3b061d53898113a1a67d4584
jodufour <jodufour@student.42.fr>
2025-02-27 03:12:07 +0100

    refactor: renamed some files in the level13 resources

--- level13/resources/get_token.sh
+++ level13/resources/get_token.sh
@@ -2,11 +2,11 @@
 
 FLAG=../flag
 
-# Compile the modified source code of the `level13` binary
-clang -Wall -Wextra -o level13.out level13.c
+# Compile the altered version of the `level13` program
+clang -Wall -Wextra -o altered_level13 altered_level13.c
 
-# Execute the `level13.out` file and save the token to a file
-./level13.out >${FLAG}
+# Execute the `altered_level13` file and save the token to a file
+./altered_level13 >${FLAG}
 
-# Remove the `level13.out` file
-rm level13.out
+# Remove the `altered_level13` file
+rm altered_level13
